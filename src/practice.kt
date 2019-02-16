c7ae432d398691d3ae18e2a39653a3cc58329f7c
borebrian <borebrian77@gmail.com>
2019-02-16 07:50:41 +0300

    Initial commit

---
+++ src/practice.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+  var list =Array<String>(6){"dsss"}
+
+
+    list[0]="bRIAN"
+    list[1]="Benaa"
+    list[2]="Kili"
+    for (elemente in list){
+        println(elemente)
+    }
+
+
+}
