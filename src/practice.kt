2383cfa89944942b326eee9741c0193f910bb45e
borebrian <borebrian77@gmail.com>
2019-02-21 10:13:43 +0300

    LIST

--- src/practice.kt
+++ src/practice.kt
@@ -1,10 +1,27 @@
 fun main(args:Array<String>) {
-    var list = mutableListOf<Int>() //immutable,fixed size read only
-    list.add(44)
-    list.add(45)
 
-
-    for (m in list) {
+    var mynumbers: List<Int> = listOf(1, 2, 3, 4, 5, 6, 7)
+    val filteredd = mynumbers.filter { it < 5 }
+    for (n in filteredd) {
+        println(n)
+    }
+    val myMap = mynumbers.map { it * 4 }
+    for (m in myMap) {
         println(m)
     }
+    val test = mynumbers.filter { it < 5 }
+        .map { it + 5 }
+    for (t in test) {
+        println(t)
+    }
+    var people = listOf<Person>(Person(1,"BRIAN"),   (Person(2,"Kili")))
+    var pple =people.map { it.name }
 }
+    class  Person(age:Int,name:String){
+
+
+    }
+
+
+
+
